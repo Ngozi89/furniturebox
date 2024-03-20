18ba4f11e0a3731f65afa205a42b62c6b4543b47
Ngozi Omenka <ngozi.madu@live.se>
2024-03-20 23:51:19 +0000

    Added furniture template content to display products

--- products/views.py
+++ products/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Furniture
 
 # Create your views here.
+def all_furnitures(request):
+    """ A view to show all furnitures, include sorting and search queries """
+
+    products = Furniture.objects.all()
+
+    context = {
+        'products':products,
+    }
+
+    return render(request, 'products/furnitures.html', context)
