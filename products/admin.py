227a997da2e15a492492868809624690750d112b
Ngozi Omenka <ngozi.madu@live.se>
2024-03-20 22:10:44 +0000

    Added product color model and customise the admin

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,32 @@
 from django.contrib import admin
-from .models import Furniture, Category
+from .models import Furniture, Category, Color
 
 # Register your models here.
-admin.site.register(Furniture)
-admin.site.register(Category)
+class FurnitureAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'itemnumber',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('itemnumber',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+class ColorAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Furniture, FurnitureAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Color, ColorAdmin)
--- products/models.py
+++ products/models.py
@@ -11,8 +11,22 @@ class Category(models.Model):
     def get_friendly_name(self):
         return self.friendly_name
 
+    class Meta:
+        verbose_name_plural = "Categories"    
+
+class Color(models.Model):
+    name = models.CharField(max_length=50)
+    friendly_name = models.CharField(max_length=50, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
 class Furniture(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    color = models.ForeignKey('Color', null=True, blank=True, on_delete=models.SET_NULL)
     itemnumber = models.CharField(max_length=200, null=True, blank=True)
     name = models.CharField(max_length=200)
     description = models.TextField()
